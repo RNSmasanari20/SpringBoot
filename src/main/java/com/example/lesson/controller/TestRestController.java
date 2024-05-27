9ade2dedfd983b11f18dffe77350f0a083ce65ee
masanari okuma <kokoakuma@gmail.com>
2024-05-28 08:25:50 +0900

    5/24日提出 完成

--- src/main/java/com/example/lesson/controller/TestRestController.java
+++ src/main/java/com/example/lesson/controller/TestRestController.java
@@ -6,9 +6,7 @@ import com.example.lesson.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +20,31 @@ public class TestRestController {
     public ResponseEntity<List<ProductRecord>> allGet(){
         List<ProductRecord> list = productService.findAll();
         return new ResponseEntity<>(list,HttpStatus.OK);
+    }
+
+    @GetMapping("/api/product/{id}")
+    public ResponseEntity<ProductRecord> recGet(@PathVariable("id") int id){
+        ProductRecord pr = productService.findById(id);
+        return new ResponseEntity<>(pr,HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/api/product")
+    public ResponseEntity<ProductRecord> madeRec(@RequestBody ProductRecord productRecord){
+        ProductRecord addRecord = new ProductRecord(-1, productRecord.name(), productRecord.price());
+        productService.insert(addRecord);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/api/product/{id}")
+    public ResponseEntity<ProductRecord> upRec(@RequestBody ProductRecord productRecord){
+        productService.update(productRecord);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/api/product/{id}")
+    public ResponseEntity<ProductRecord> delRec(@PathVariable("id") int id,@RequestBody ProductRecord productRecord){
+        productService.delete(productRecord.id());
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
---
+++ src/main/resources/static/product.js
@@ -0,0 +1,58 @@
+<script>
+    'use strict';
+
+    fetch(`/api/products`)
+      .then(res => {
+        if(res.status === 400) {
+          document.getElementById('id').textContent = "";
+          document.getElementById('name').textContent = "";
+          document.getElementById('price').textContent = "";
+        } else {
+            res.json()
+            .then(data => {
+
+            const trTh = document.createElement('tr');
+
+            const thId = document.createElement('th');
+            thId.textContent = 'id';
+            trTh.appendChild(thId);
+
+            const thName = document.createElement('th');
+            thName.textContent = '名前';
+            trTh.appendChild(thName);
+
+            const thPrice = document.createElement('th');
+            thPrice.textContent = '値段';
+            trTh.appendChild(thPrice);
+
+            document.getElementById('tableMain').appendChild(trTh);
+
+            for(let i=0; i<data.length; i++){
+
+            let trCon = document.createElement('tr');
+
+            let trId = document.createElement('tr');
+            trId.textContent = data[i].id;
+            trCon.appendChild(trId);
+
+            let input_radio = document.createElement("input");
+            input_radio.type = "radio";
+            input_radio.name = "product_radio";
+            trId.appendChild(input_radio);
+
+            let trName = document.createElement('th');
+            trName.textContent = data[i].name;
+            trCon.appendChild(trName);
+
+            let trPrice = document.createElement('th');
+            trPrice.textContent = data[i].price;
+            trCon.appendChild(trPrice);
+
+            document.getElementById('tableMain').appendChild(trCon);
+             }
+            })
+        }
+      });
+
+
+</script>
